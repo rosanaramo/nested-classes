c6d1e53cb00d310ad6cb2d91429503d917385650
Rosana Ramo Santos <rosanaramo4@gmail.com>
2023-10-25 15:28:19 -0300

    created project to study nested classes and lambda

---
+++ src/main/java/EnclosingClass.java
@@ -0,0 +1,24 @@
+public class EnclosingClass {
+   static int countStatic =0;
+   int countNonStatic = 0;
+
+   public class NestedClass{
+      void printStatic(){
+         System.out.println(countStatic);
+      }
+   }
+
+   /***
+    * This nested class cannot access non-static members
+    */
+   static class StaticNestedClass{
+        public void doInner(){
+           System.out.println(countStatic);
+        }
+   }
+
+   public static void main(String[] args) {
+      EnclosingClass enclosingClass = new EnclosingClass();
+      NestedClass nestedClass = enclosingClass.new NestedClass();
+   }
+}
